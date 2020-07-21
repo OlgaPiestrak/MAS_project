948b236127b1e45775555178111553a75a0e22ba
Vincent Koeman <koeman@chainels.com>
2020-07-21 15:30:29 +0200

    persist the user configuration in Redis

--- data/www/src/controllers/register_user.py
+++ data/www/src/controllers/register_user.py
@@ -12,9 +12,12 @@ if __name__ == '__main__':
     cert_file = dirname(abspath(__file__)) + '/cert.pem'
     # FIXME: use server-env or similar on actual cloud for authentication
     redis = Redis(host='172.16.238.12', ssl=True, ssl_ca_certs=cert_file, password='changemeplease')
-    print(redis.acl_setuser(enabled=True, username=args.username, passwords=['+' + args.password],
-                            categories=['+@all', '-@dangerous'], keys=['user:' + args.username, args.username + '-*',
-                                                                       'emotion_detection', 'intent_detection',
-                                                                       'people_detection', 'face_recognition',
-                                                                       'robot_memory']))
+    pipe = redis.pipeline()
+    pipe.acl_setuser(enabled=True, username=args.username, passwords=['+' + args.password],
+                     categories=['+@all', '-@dangerous'], keys=['user:' + args.username, args.username + '-*',
+                                                                'emotion_detection', 'intent_detection',
+                                                                'people_detection', 'face_recognition',
+                                                                'robot_memory'])
+    pipe.acl_save()
+    print(pipe.execute())
     redis.close()
