0c0bd72c1948ee474ebca657df2a461ecb7a87e6
Vincent Koeman <koeman@chainels.com>
2020-07-31 10:14:27 +0200

    use own php7.4 image > each start-up will be faster now

--- autostart/composer-install.sh
+++ autostart/composer-install.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
-apt-get -y update
-apt-get install -y --fix-missing sshpass
-
 cd /var/www/default
-composer self-update && composer install --no-dev --no-suggest --no-progress --prefer-dist --optimize-autoloader
-
-pip2 install redis hiredis
+composer install --no-dev --no-suggest --no-progress --prefer-dist --optimize-autoloader
