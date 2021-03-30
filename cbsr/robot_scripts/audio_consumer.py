ac155158db954d81d70bf6c5666c8a42f54b6065
Vincent Koeman <vj.koeman@quicknet.nl>
2021-03-30 12:48:32 +0200

    robot scripts updates

--- cbsr/robot_scripts/audio_consumer.py
+++ cbsr/robot_scripts/audio_consumer.py
@@ -1,8 +1,8 @@
 import os
 from argparse import ArgumentParser
-from shutil import rmtree
 from tempfile import NamedTemporaryFile
-from threading import Thread
+from threading import Thread, Timer
+from time import sleep
 
 from cbsr.device import CBSRdevice
 from qi import Application
@@ -15,10 +15,9 @@ class RobotAudio(CBSRdevice):
         self.language = session.service('ALDialog')
         self.audio_player = session.service('ALAudioPlayer')
 
-        # create a folder on robot to temporarily store loaded audio files
-        self.audio_folder = os.path.join(os.getcwd(), 'sounds')
-        if not (os.path.exists(self.audio_folder)):
-            os.mkdir(self.audio_folder)
+        self.is_audio_playing = False
+        self.loaded_audio = {}
+        self.backup_timer = None
 
         self.topics = topics
         super(RobotAudio, self).__init__(server, username, password, profiling)
@@ -55,28 +54,17 @@ class RobotAudio(CBSRdevice):
             self.produce('LanguageChanged')
         elif channel == 'action_load_audio':
             self.produce('LoadAudioStarted')
-            audio_file = self.store_audio(data)
-            audio_id = self.audio_player.loadFile(audio_file)
+            audio_id = self.load_audio(data, True)
             self.publish('robot_audio_loaded', audio_id)
             self.produce('LoadAudioDone')
         elif channel == 'action_play_audio':
-            self.audio_player.stopAll()
-            try:
-                loaded = int(data)
-                self.produce('PlayAudioStarted')
-                self.audio_player.play(loaded)
-                self.produce('PlayAudioDone')
-            except:
-                audio_file = self.store_audio(data)
-                self.produce('PlayAudioStarted')
-                self.audio_player.playFile(audio_file)
-                self.produce('PlayAudioDone')
-                os.remove(audio_file)
+            self.start_audio(data)
         elif channel == 'action_clear_loaded_audio':
             self.produce('ClearLoadedAudioStarted')
             self.audio_player.unloadAllFiles()
-            rmtree(self.audio_folder)
-            os.mkdir(self.audio_folder)
+            for audio_info in self.loaded_audio.values():
+                os.remove(audio_info['file'])
+            self.loaded_audio = {}
             self.produce('ClearLoadedAudioDone')
         elif channel == 'action_speech_param':
             params = data.split(';')
@@ -87,6 +75,56 @@ class RobotAudio(CBSRdevice):
         else:
             print('Unknown command')
 
+    def start_audio(self, data):
+        self.audio_player.stopAll()
+        try:
+            audio = int(data)
+        except ValueError:
+            audio = self.load_audio(data, False)
+
+        self.start_backup_timer(audio)
+        self.is_audio_playing = True
+        self.produce('PlayAudioStarted')
+        self.audio_player.play(audio)
+        self.is_audio_playing = False
+        self.stop_audio(audio)
+
+    def stop_audio(self, audio):
+        if self.backup_timer:
+            self.backup_timer.cancel()
+            self.backup_timer = None
+
+        if self.is_audio_playing:
+            self.audio_player.stopAll()
+            self.is_audio_playing = False
+
+        self.produce('PlayAudioDone')
+
+        if not self.loaded_audio[audio]['keep']:
+            self.audio_player.unloadFile(audio)
+            os.remove(self.loaded_audio[audio]['file'])
+            del self.loaded_audio[audio]
+
+    def load_audio(self, data, keep):
+        audio_file = self.store_audio(data)
+        audio_id = self.audio_player.loadFile(audio_file)
+        self.loaded_audio[audio_id] = {'file': audio_file, 'keep': keep}
+        return audio_id
+
+    def start_backup_timer(self, audio):
+        duration = 0.0
+        # audio_player.getFileLength sometimes needs more than one try to get the right duration.
+        # Max. 5 attempts given.
+        for i in range(0, 5):
+            duration = self.audio_player.getFileLength(audio)
+            if duration > 0.0:
+                break
+            sleep(0.1)
+        if duration <= 0.0:
+            raise ValueError('Duration of sound file should be longer than 0.0')
+        self.backup_timer = Timer(duration + 1.0, self.stop_audio, audio)
+        self.backup_timer.start()
+
     def change_language(self, value):
         if value == 'nl-NL':
             self.language.setLanguage('Dutch')
@@ -95,7 +133,7 @@ class RobotAudio(CBSRdevice):
 
     @staticmethod
     def store_audio(data):
-        audio_location = NamedTemporaryFile().name
+        audio_location = NamedTemporaryFile(prefix='audio_', suffix='.wav').name
         with open(audio_location, 'wb') as f:
             f.write(data)
         return audio_location
