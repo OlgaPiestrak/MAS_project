be2454db42d418654c365a9c617975a8d9af4992
Vincent Koeman <koeman@chainels.com>
2021-03-31 13:18:02 +0200

    updated robot-memory-service

--- cbsr/robot_memory/robot_memory_service.py
+++ cbsr/robot_memory/robot_memory_service.py
@@ -1,8 +1,7 @@
 from datetime import datetime
-
-from cbsr.service import CBSRservice
 from redis import DataError
 from simplejson import loads
+from cbsr.service import CBSRservice
 
 
 class EntryIncorrectFormatError(Exception):
@@ -10,8 +9,8 @@ class EntryIncorrectFormatError(Exception):
     pass
 
 
-class UserDoesNotExistError(Exception):
-    """Raised when a database operation is attempted on a non existing user"""
+class InteractantDoesNotExistError(Exception):
+    """Raised when a database operation is attempted on a non existing interactant"""
     pass
 
 
@@ -23,79 +22,109 @@ class RobotMemoryService(CBSRservice):
         return ['robot']
 
     def get_channel_action_mapping(self):
-        return {self.get_full_channel('memory_add_entry'): self.entry_handler,
-                self.get_full_channel('memory_user_session'): self.get_user_session,
-                self.get_full_channel('memory_set_user_data'): self.set_user_data,
-                self.get_full_channel('memory_get_user_data'): self.get_user_data}
+        return {self.get_full_channel('memory_add_entry'): self.add_entry,
+                self.get_full_channel('memory_set_session'): self.set_session,
+                self.get_full_channel('memory_set_interactant_data'): self.set_interactant_data,
+                self.get_full_channel('memory_get_interactant_data'): self.get_interactant_data,
+                self.get_full_channel('memory_delete_interactant'): self.delete_interactant,
+                self.get_full_channel('memory_delete_all_interactants'): self.delete_all_interactants}
 
-    def get_user_session(self, message):
+    def set_session(self, message):
+        """Called to indicate that a new session has started.
+        If a interactant with interactant_id does not exist a new one is created."""
         try:
-            # retrieve data from message
-            user_key = 'user:' + self.get_data(message, 1, correct_format='user_id')[0]
-
+            interactant_id, session_id = self.get_data(message, 2, correct_format='interactant_id;session_id')
+            interactant_key = self.get_interactant_key(interactant_id)
             timestamp = str(datetime.now())
-            # check if user exists and if they exist return their session number
-            if self.redis.exists(user_key):
-                with self.redis.pipeline() as pipe:
-                    pipe.hincrby(user_key, 'session_number', amount=1)
-                    pipe.hset(user_key, 'last_interaction', timestamp)
-                    result = pipe.execute()
-                self.produce_data('session_number', str(result[0]))
-            # if user does not exist, create it, and return the session number of 1 (first session)
-            else:
-                self.redis.hmset(user_key, {'creation_date': timestamp,
-                                            'last_interaction': timestamp,
-                                            'session_number': '1'})
-                self.produce_data('session_number', '1')
-        except (EntryIncorrectFormatError, DataError) as err:
-            print(self.identifier + ' > Could not retrieve user session: ' + err.message)
+            interactant_data = {'session_id': session_id,
+                                'last_interaction': timestamp}
 
-    def entry_handler(self, message):
+            # in case of a new interactant, one is created with now() as creation_date
+            if not self.redis.exists(interactant_key):
+                interactant_data.update({'creation_date': timestamp})
+
+            # Update interactant data or create new interactant with data
+            self.redis.hmset(interactant_key, interactant_data)
+            self.produce_event('SessionSet')
+        except (EntryIncorrectFormatError, DataError) as err:
+            print(self.identifier + ' > Could not start a new session: ' + str(err))
+
+    def add_entry(self, message):
         try:
             # retrieve data from message
-            data = self.get_data(message, 3, 'user_id;entry_name;entry')
-            # a user needs to exist to link the entry to.
-            user_key = 'user:' + data[0]
-            if not (self.redis.exists(user_key)):
-                raise UserDoesNotExistError('User with ID ' + user_key + 'does not exist')
+            interactant_id, entry_type, entry_data = self.get_data(message, 3, 'interactant_id;entry_name;entry')
+            # a interactant needs to exist to link the entry to.
+            interactant_key = self.get_interactant_key(interactant_id)
+            if not (self.redis.exists(interactant_key)):
+                raise InteractantDoesNotExistError('Interactant with ID ' + interactant_id + 'does not exist')
 
             # generate the latest hash id for this particular entry type
-            entry_key = data[1]
-            count = self.redis.hincrby(user_key, entry_key, 1)
-            hash_name = entry_key + ':' + data[0] + ':' + str(count)
+            count = self.redis.hincrby(interactant_key, 'entry_type:' + entry_type, 1)
+            hash_name = self.get_user_id() + ':' + interactant_id + ':entry:' + entry_type + ':' + str(count)
 
             # the supplied data needs to have the form a a dict.
             entry = {}
-            for item in loads(data[2]):
+            for item in loads(entry_data):
                 entry.update(item)
             entry.update({'datetime': str(datetime.now())})  # timestamp the entry
 
-            # store the entry dict as a hash in redis with hash name: entry_type:user_id:entry_id
+            # store the entry dict as a hash in redis with hash name: user_id:interactant_id:entry:entry_type:entry_id
             self.redis.hmset(hash_name, entry)
             self.produce_event('MemoryEntryStored')
         except(ValueError, SyntaxError, EntryIncorrectFormatError) as err:
-            print(self.identifier + ' > Memory entry does not have the right format: ' + err.message)
-        except (UserDoesNotExistError, DataError) as err:
-            print(self.identifier + ' > The database action failed: ' + err.message)
+            print(self.identifier + ' > Memory entry does not have the right format: ' + str(err))
+        except (InteractantDoesNotExistError, DataError) as err:
+            print(self.identifier + ' > The database action failed: ' + str(err))
 
-    def set_user_data(self, message):
+    def set_interactant_data(self, message):
         try:
-            data = self.get_data(message, 3, 'user_id;key;value')
-            self.redis.hset('user:' + data[0], data[1], data[2])
-            self.produce_event('UserDataSet')
+            interactant_id, key, value = self.get_data(message, 3, 'interactant_id;key;value')
+            self.redis.hset(self.get_interactant_key(interactant_id), key, value)
+            self.produce_event('InteractantDataSet')
         except (EntryIncorrectFormatError, DataError) as err:
-            print(self.identifier + ' > User data could not be set due to: ' + err.message)
+            print(self.identifier + ' > Interactant data could not be set due to: ' + str(err))
 
-    def get_user_data(self, message):
+    def get_interactant_data(self, message):
         try:
-            data = self.get_data(message, 2, 'user_id;key')
-            value = self.redis.hget('user:' + data[0], data[1])
-            self.produce_data(data[1], value)
+            interactant_id, key = self.get_data(message, 2, 'interactant_id;key')
+            value = self.redis.hget(self.get_interactant_key(interactant_id), key)
+            if value:
+                self.produce_data(key, value.decode('utf-8'))
+            else:
+                self.produce_data(key, None)
         except EntryIncorrectFormatError as err:
-            print(self.identifier + ' > Could not get user data due to: ' + err.message)
+            print(self.identifier + ' > Could not get interactant data due to: ' + str(err))
+
+    def delete_interactant(self, message):
+        try:
+            # retrieve data from message
+            interactant_id = self.get_data(message, 1, correct_format='interactant_id')[0]
+            # get all entries attached to this interactant
+            entries = list(self.redis.scan_iter(self.get_user_id() + ':' + interactant_id + ':entry:*'))
+            # add reference to interactant
+            entries.append(self.get_interactant_key(interactant_id))
+            # delete all entries and interactant
+            self.redis.delete(*entries)
+            self.produce_event('InteractantDeleted')
+        except EntryIncorrectFormatError as err:
+            print(self.identifier + ' > Could not delete interactant due to: ' + str(err))
+
+    def delete_all_interactants(self, message):
+        try:
+            # get all interactants and related entries
+            interactants = list(self.redis.scan_iter(self.get_interactant_key('*')))
+            entries = list(self.redis.scan_iter(self.get_user_id() + ':*:entry:*'))
+            # delete all interactants and related entries
+            self.redis.delete(*(interactants + entries))
+            self.produce_event('AllInteractantsDeleted')
+        except DataError as err:
+            print(self.identifier + ' > Could not delete all interactants due to: ' + str(err))
 
     def produce_data(self, key, value):
         self.publish('memory_data', str(key) + ';' + str(value))
+
+    def get_interactant_key(self, interactant_id):
+        return self.get_user_id() + ':interactant:' + interactant_id
 
     @staticmethod
     def get_data(message, correct_length, correct_format=''):
