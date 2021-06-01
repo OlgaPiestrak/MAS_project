7626fb4b8c5d0caeacb37cac53e9d974cace1eba
Vincent Koeman <vj.koeman@quicknet.nl>
2021-06-01 10:40:04 +0200

    lower the setAngles max speed

--- cbsr/robot_scripts/action_consumer.py
+++ cbsr/robot_scripts/action_consumer.py
@@ -255,7 +255,7 @@ class RobotConsumer(CBSRdevice):
             if times:
                 self.motion.angleInterpolation(joints, angles, times, True)
             else:
-                self.motion.setAngles(joints, angles, 1.0)
+                self.motion.setAngles(joints, angles, 0.1)
             self.produce('PlayMotionDone')
         except ValueError as valerr:
             print('action_play_motion received incorrect input: ' + valerr.message)
